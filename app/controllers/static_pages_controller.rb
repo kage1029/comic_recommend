2203d0d60a4915ef639dada3927e55cb2b0037d3
kage1029 <ichika789rider@gmail.com>
2020-03-21 18:53:12 +0900

    Finished static-gpages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,12 +2,15 @@ class StaticPagesController < ApplicationController
   def home
   end
 
-  def help
-  end
-
   def about
   end
 
   def contact
+  end
+
+  def use_policy
+  end
+  
+  def privacy_policy
   end
 end
